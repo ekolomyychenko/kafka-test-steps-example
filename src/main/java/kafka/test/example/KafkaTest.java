54e6b58beed250cc2837f7eecfca4711da830996
evgeniakolomyychenko <kolomyychenko@mango.rocks>
2020-04-02 21:57:54 +0300

    refactor

--- src/main/java/kafka/test/example/KafkaTest.java
+++ src/main/java/kafka/test/example/KafkaTest.java
@@ -2,37 +2,42 @@ package kafka.test.example;
 
 import org.apache.kafka.clients.consumer.Consumer;
 import org.apache.kafka.clients.producer.Producer;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
 import org.testcontainers.containers.KafkaContainer;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
 
 public class KafkaTest {
 
+    @Rule
+    public KafkaContainer kafka = new KafkaContainer()
+            .withExposedPorts(9092, 9093, 2181)
+            .withEmbeddedZookeeper();
+
+    private KafkaClient kafkaClient = new KafkaClient();
 
     @Test
     public void kafka_test() throws ExecutionException, InterruptedException, IOException {
-        KafkaContainer kafka = new KafkaContainer()
-                .withExposedPorts(9092, 9093, 2181)
-                .withEmbeddedZookeeper();
-
-        kafka.start();
-
-        KafkaClient kafkaClient = new KafkaClient();
 
         KafkaClient.BOOTSTRAP_SERVERS = kafka.getBootstrapServers();
-
         kafkaClient.createTopics();
 
         Producer producer = kafkaClient.createProducer();
         kafkaClient.send(producer, "some-test-topic", "Hello");
+        kafkaClient.send(producer, "some-test-topic", "World");
 
         Consumer consumer = kafkaClient.createConsumer("some-test-topic");
-        List<String> messages = kafkaClient.getMessages(consumer);
+        List<String> messagesActual = kafkaClient.getMessages(consumer);
 
-        System.out.println(messages);
+        List<String> messagesExpected = new ArrayList<>();
+        messagesExpected.add("Hello");
+        messagesExpected.add("World");
+
+        Assertions.assertEquals(messagesExpected, messagesActual);
     }
-
 }
