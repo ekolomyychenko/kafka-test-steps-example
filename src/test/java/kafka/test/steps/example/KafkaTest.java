84327caf672d4072ff45dcc71d4ba90249441686
evgeniakolomyychenko <kolomyychenko@mango.rocks>
2020-09-24 09:05:08 +0300

    refactor

--- src/test/java/kafka/test/steps/example/KafkaTest.java
+++ src/test/java/kafka/test/steps/example/KafkaTest.java
@@ -2,19 +2,21 @@ package kafka.test.steps.example;
 
 import org.apache.kafka.clients.consumer.Consumer;
 import org.apache.kafka.clients.producer.Producer;
-import org.junit.Rule;
-import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.testcontainers.containers.KafkaContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
 
+@Testcontainers
 public class KafkaTest {
 
-    @Rule
+    @Container
     public KafkaContainer kafka = new KafkaContainer()
             .withEmbeddedZookeeper();
 
@@ -33,6 +35,8 @@ public class KafkaTest {
         Consumer consumer = kafkaClient.createConsumer("some-test-topic");
         List<String> messagesActual = kafkaClient.getMessages(consumer);
 
+        System.out.println(messagesActual);
+        
         List<String> messagesExpected = new ArrayList<>();
         messagesExpected.add("Hello");
         messagesExpected.add("World");
